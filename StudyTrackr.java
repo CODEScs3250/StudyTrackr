29ad7bfecdc50549cb9ed5f245a4fa870001c044
bfcortesss <briancortes0323@gmail.com>
2025-11-17 15:24:21 -0700

    Current work for #Issue14. Creation of Welcome banner.

---
+++ StudyTrackr.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+* StudyTrackr
+* Is a software that provides students an effective way to
+* organize their study sessions, manage subjects, and track
+* progress over time.
+*
+* @author CODEcs3250 ( organization working on overall project).
+* @author Brian Flores
+* @version 1.0 (Initial working program for iteration 1).
+*/
+
+public class StudyTrackr {
+
+public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+boolean running = true;
+
+/*
+*
+* Welcome Banner.
+*
+*/
+
+System.out.println("-------------------------");
+System.out.println("|WELCOME TO StudyTrackr!|");
+System.out.println("-------------------------");
+
+
+}
+
+}
