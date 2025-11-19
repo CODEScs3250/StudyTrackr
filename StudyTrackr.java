6b3903b21dcecdb4bc8f166a2b7cfd274b8b801a
bfcortesss <briancortes0323@gmail.com>
2025-11-19 13:51:01 -0700

    Added loops and menu display option.

--- StudyTrackr.java
+++ StudyTrackr.java
@@ -28,7 +28,39 @@ System.out.println("-------------------------");
 System.out.println("|WELCOME TO StudyTrackr!|");
 System.out.println("-------------------------");
 
+// Main Loop implemenation //
+while (running) {
+  case "1":
+	System.out.println("\n[Start Study Session] Coming soon...\n");
+	break;
 
+  case "2":
+        System.out.println("\n[View Tasks] Coming soon...\n");
+        break;
+
+  case "3":
+        System.out.println("\n[Add Tasks] Coming soon...\n");
+        break;
+
+  case "4":
+        System.out.println("\nExiting StudyTrackr. Goodbye!\n");
+        running = false;
+	break;
+}
+
+}
+
+input.close();
+
+}
+
+// Prints the main menu // 
+private static printMainmenu() {
+  System.out.println("Main Menu:");
+  System.out.println(" 1. Start Study Session");
+  System.out.println(" 2. View Tasks");
+  System.out.println(" 3. Add Tasks");
+  System.out.println(" 4. Exit\n");
 }
 
 }
